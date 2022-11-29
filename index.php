94790da1ef937d98445b4a80ff9070df153aa969
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-11-29 19:49:05 +0100

    Added layout and control with alert

--- index.php
+++ index.php
@@ -4,41 +4,49 @@
 // $numbers = '0123456789';
 // $symbols = '!$%&/()=?*+-#';
 
+include __DIR__ . '/partials/head.php';
 include __DIR__ . '/functions/functions.php';
 
 $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
 $password = '';
 $n = 1;
 
-if (isset($_GET['pw-length'])) {
+if (isset($_GET['pw-length']) && !empty($_GET['pw-length'])) {
     $password = createPassword($_GET['pw-length']);
 }
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Password Generator</title>
-</head>
-
 <body>
     <div class="wrapper text-center">
+
+        <?php if (isset($_GET['pw-length'])) {
+            if (empty($_GET['pw-length'])) { ?>
+        <div class="alert alert-danger">
+            Nessun parametro valido inserito
+        </div>
+        <?php }
+        }
+        ?>
+
         <h1>Strong Password Generator</h1>
         <h2>Genera una password sicura</h2>
 
         <div class="container">
             <form action="index.php" method="GET" name="password-generator">
-                <label for="pw-length" class="form-label">Lunghezza password:</label>
-                <input type="number" id="pw-length" name="pw-length" class="form-control">
-                <button type="submit">Invia</button>
-                <button type="reset">Annulla</button>
+                <div class="row pt-5">
+                    <div class="col-6">
+                        <label for="pw-length" class="form-label">Lunghezza password:</label>
+                    </div>
+                    <div class="col-6">
+                        <input type="number" id="pw-length" name="pw-length" class="form-control">
+                    </div>
+                </div>
+
+                <div class="mt-5">
+                    <button type="submit">Invia</button>
+                    <button class="ms-3" type="reset">Annulla</button>
+                </div>
             </form>
         </div>
     </div>
--- result.php
+++ result.php
@@ -1,6 +1,23 @@
 <?php
+include __DIR__ . '/partials/head.php';
+
 session_start();
-
-echo "<h3>Ecco la tua nuova password:</h3> {$_SESSION['newPassword']}";
-
 ?>
+
+
+<div class="wrapper pt-5">
+
+    <div class="container margin-top text-center">
+        <h3>Ecco la tua nuova password:</h3>
+
+        <div class="text">
+            <?php
+            echo $_SESSION['newPassword'];
+            ?>
+        </div>
+    </div>
+
+</div>
+</body>
+
+</html>
